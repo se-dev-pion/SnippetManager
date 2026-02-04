e29efb4525d9f5b827ae39b49a2515d6cab60eea
Felix <KLDXYO@outlook.com>
2026-02-04 21:05:36 +0800

    chore: 添加配置文件命令

---
+++ src/commands/addConfig.ts
@@ -0,0 +1,64 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as xml from "xml2js";
+import * as refreshViews from "../tools/refreshViews";
+
+export function main(
+  loadedView: vscode.TreeItem[],
+  refresh: refreshViews.main,
+  context: vscode.ExtensionContext,
+) {
+  const main = vscode.commands.registerCommand(
+    "snippetManager.addConfig",
+    async () => {
+      const file = await vscode.window.showOpenDialog({
+        title: "选择配置文件(.xml)",
+        canSelectFiles: true,
+        canSelectFolders: false,
+        canSelectMany: false,
+        filters: {
+          配置文件: ["xml"],
+          所有文件: ["*"],
+        },
+      });
+      if (!(file && file.length > 0)) {
+        return;
+      }
+      for (let item of loadedView) {
+        if (item.description === file[0].fsPath) {
+          vscode.window.showWarningMessage("此文件已经存在!");
+          return;
+        }
+      }
+
+      const fileContent = await fs.readFileSync(file[0].fsPath, {
+        encoding: "utf-8",
+      });
+      const result = await xml.parseStringPromise(fileContent);
+      if (
+        !(
+          result &&
+          result.root &&
+          result.root.name &&
+          result.root.item
+        )
+      ) {
+        vscode.window.showWarningMessage(
+          "配置文件必须包含:root & root/name & root/snippetManagerConfig & root/item属性!",
+        );
+        return;
+      }
+      const newItem = new vscode.TreeItem(result.root.name[0]);
+      newItem.description = file[0].fsPath;
+      newItem.contextValue = "configItem";
+      newItem.command = {
+        command: "vscode.open",
+        title: "打开文件",
+        arguments: [vscode.Uri.file(file[0].fsPath)],
+      };
+      loadedView.push(newItem);
+      refresh.loaded();
+    },
+  );
+  context.subscriptions.push(main);
+}
