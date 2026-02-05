370d08153c120556fea7a5c4c50b40697ccb2a20
Felix <KLDXYO@outlook.com>
2026-02-05 11:34:54 +0800

    feat: 更改了运行顺序;添加了高亮函数

--- src/extension.ts
+++ src/extension.ts
@@ -6,20 +6,20 @@ import * as reOpenConfig from "./commands/reOpenConfig";
 import * as compiler from "./commands/compiler";
 
 import * as refreshViews from "./tools/refreshViews";
+import * as highlight from "./tools/highlight";
 
 import * as complete from "./other/complete";
 import * as typeComplete from "./other/typeComplete";
 
-export function activate(context: vscode.ExtensionContext) {
-  const refresh = new refreshViews.main();
-  const loadedView = refresh.loaded();
+export async function activate(context: vscode.ExtensionContext) {
+  const refresh = new refreshViews.Main();
+  const loadedView = await refresh.loaded();
 
   addConfig.main(loadedView, refresh, context);
   delConfig.main(loadedView, refresh, context);
   reOpenConfig.main(loadedView, refresh, context);
   compiler.main(context);
-
-  refresh.loaded();
+  highlight.main();
 
   complete.main(loadedView, context);
   typeComplete.main(context);
